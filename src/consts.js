89c6b307a424468465952d0ff5df6a72318140fb
RK Anik <rkanik.me@gmail.com>
2021-08-21 22:23:10 +0600

    Removed firebase

--- src/consts.js
+++ src/consts.js
@@ -3,7 +3,6 @@ export const _googleMapApiKey = process.env.VUE_APP_GMAP_API_KEY
 export const _firebaseConfig = {
 	apiKey: process.env.VUE_APP_FB_API_KEY,
 	authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
-	databaseURL: process.env.VUE_APP_FB_DATABASE_URL,
 	projectId: process.env.VUE_APP_FB_PROJECT_ID,
 	storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
 	messagingSenderId: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
